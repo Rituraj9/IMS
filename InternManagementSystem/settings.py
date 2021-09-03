7d92c18a490caa6f5440bfd4b4632ab3d2a044da
Rituraj <46903942+Rituraj9@users.noreply.github.com>
2021-09-03 20:35:10 +0530

    Update settings.py

--- InternManagementSystem/settings.py
+++ InternManagementSystem/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd9bm=ankh66ye=f65y7g0#nzz*v)jyh5hg-*p$8@#t==niwle('
+#SECRET_KEY = 'd9bm=ankh66ye=f65y7g0#nzz*v)jyh5hg-*p$8@#t==niwle('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
