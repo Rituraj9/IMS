5cd5844b2483d1be30b7f542832d423426eccf0a
Rituraj <46903942+Rituraj9@users.noreply.github.com>
2021-10-01 01:29:07 +0530

    Update settings.py

--- InternManagementSystem/settings.py
+++ InternManagementSystem/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'd9bm=ankh66ye=f65y7g0#nzz*v)jyh5hg-*p$8@#t==niwle('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['internmanagementsystem.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
