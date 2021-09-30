f3b8e13613bdb1bf2735cc60717e7bf0a6ca066d
Rituraj <46903942+Rituraj9@users.noreply.github.com>
2021-10-01 00:22:14 +0530

    Update settings.py

--- InternManagementSystem/settings.py
+++ InternManagementSystem/settings.py
@@ -5,7 +5,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+heroku config:set DISABLE_COLLECTSTATIC=1
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
