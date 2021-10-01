859ec55f4a8301496b8ad6f473185fe540a77383
Abhinav Kumar <46753270+Avikr76@users.noreply.github.com>
2021-10-01 12:44:05 +0530

    Delete download resume

--- candidates/views.py
+++ candidates/views.py
@@ -269,11 +269,5 @@ def remove_job(request, slug):
     return redirect('applied-jobs')
     #return HttpResponseRedirect('/job/{}'.format(job.slug))
 
-def download(request, path):
-    file_path = os.path.join(settings.MEDIT_ROOT, path)
-    if os.path.exists(file_path):
-        with open(file_path, 'rb') as fh:
-            response = HttpResponse(fh.read(), content_type="application/resume")
-            response['Content-Disposition'] = 'inline;filename='+os.path.basename(file_path)
-            return response
-    raise Http404        
+
+ 
