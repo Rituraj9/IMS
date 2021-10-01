6a0f447831ed231fa813c81b61f3b310f288a8f7
Rituraj <46903942+Rituraj9@users.noreply.github.com>
2021-10-01 12:11:43 +0530

    Update urls.py

--- management/urls.py
+++ management/urls.py
@@ -14,10 +14,10 @@ urlpatterns = [
     path('candidate/candidate_register/', views.candidate_register, name='candidate_register'),
     path('recruiter/recruiter_register/', views.recruiter_register, name='recruiter_register'),
     
-    path('/token' , views.token_send , name="token_send"),
-    path('/success' , views.success , name='success'),
+    path('token' , views.token_send , name="token_send"),
+    path('success' , views.success , name='success'),
     path('verify/<auth_token>' , views.verify , name="verify"),
-    path('/error' , views.error_page , name="error"),
+    path('error' , views.error_page , name="error"),
     
     path('logout/', views.pagelogout, name='logout'),
    # path('activate/<uidb64>/<token>', views.activate, name='activate')
