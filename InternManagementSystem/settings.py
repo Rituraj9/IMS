af8bf882164e3d4698cec426f761147340df1ff7
Rituraj <46903942+Rituraj9@users.noreply.github.com>
2021-10-01 12:33:18 +0530

    Update settings.py

--- InternManagementSystem/settings.py
+++ InternManagementSystem/settings.py
@@ -131,8 +131,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'riiir9346@gmail.com'
-EMAIL_HOST_PASSWORD = 'Xyzrr@2021'
+EMAIL_HOST_USER = 'ritbit991@gmail.com'
+EMAIL_HOST_PASSWORD = 'bitrr@2021'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
