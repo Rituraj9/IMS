49d31576d025f718820788960c0df0cad00d3c13
Rituraj <46903942+Rituraj9@users.noreply.github.com>
2021-10-01 11:20:43 +0530

    Update urls.py

--- management/urls.py
+++ management/urls.py
@@ -8,7 +8,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 
-    path('/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('recruiter/recruiter_login/', views.recruiter_login, name='recruiter_login'),
     path('candidate/candidate_login/', views.candidate_login,name='candidate_login'),
     path('candidate/candidate_register/', views.candidate_register, name='candidate_register'),
